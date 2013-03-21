c664a2d9f6e4015d49e16881f66c4834437fa631
Lee Boynton <lee@lboynton.com>
2013-03-21 22:33:24 +0000

    Get redis blpop working and broadcasting

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-// get rid of RedisStore. Make as simple as possible by subscribing to redis ourself.
-// compare pubsub and blocking pop.
-
-var RedisStore = require('socket.io/lib/stores/redis')
-  , redis  = require('socket.io/node_modules/redis')
-  , pub    = redis.createClient()
-  , sub    = redis.createClient()
-  , client = redis.createClient()
-  , app    = require('express')()
-  , server = require('http').createServer(app)
-  , io     = require('socket.io').listen(server);
+var redis   = require('socket.io/node_modules/redis').createClient()
+  , app     = require('express')()
+  , server  = require('http').createServer(app)
+  , io      = require('socket.io').listen(server)
+  , sockets = [];
 
 server.listen(3000);
 
@@ -16,19 +10,18 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-store = new RedisStore({
-  redisPub : pub
-, redisSub : sub
-, redisClient : client
-});
-
-io.set('store', store);
-
 io.sockets.on('connection', function(socket) {
   console.log('Client connected');
-  sub.subscribe('news', function(a,b,c)
-  {
-    console.log(a,b,c);
-//    socket.emit('news', news);
-  });
 });
+
+getNews();
+
+function getNews()
+{
+  redis.blpop('news', 0, function(err, news)
+  {
+    console.log('news', news);
+    io.sockets.emit('news', news);
+    process.nextTick(getNews);
+  });
+}
