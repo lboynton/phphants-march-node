b0014289857f1b81e7c8895c7449dedca03eb01d
Lee Boynton <lee@lboynton.com>
2013-03-22 21:57:24 +0000

    Parse PHP session cookie and get data from memcached.

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
-var redis   = require('socket.io/node_modules/redis').createClient()
-  , express = require('express')
-  , app     = express()
-  , server  = require('http').createServer(app)
-  , io      = require('socket.io').listen(server)
-  , sockets = [];
+var redis    = require('socket.io/node_modules/redis').createClient()
+  , memcache = require('memcache')
+  , express  = require('express')
+  , app      = express()
+  , server   = require('http').createServer(app)
+  , io       = require('socket.io').listen(server)
+  , cookie   = require('cookie')
+  , sockets  = [];
 
+// connect to memcache on default host/port
+mcClient = new memcache.Client()
+mcClient.connect();
+
+// run HTTP server on this port
 server.listen(3000);
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+io.set('authorization', function(data, callback)
+{
+  var cookies = cookie.parse(data.headers.cookie);
+  console.log(cookies);
+  mcClient.get('sessions/' + cookies.PHPSESSID, function(error, result)
+  {
+    if (error)
+    {
+      console.log('error');
+      callback(error, false);
+    }
+    else if (result)
+    {
+      data.session = JSON.parse(result);
+      callback(null, true);
+    }
+    else
+    {
+      callback('Could not find session ID ' + cookies.PHPSESSID + ' in memcached', false);
+    }
+  });
 });
 
 io.sockets.on('connection', function(socket) {
-  console.log('Client connected');
+  var session = socket.handshake.session;
+  console.log('Received connection from user:', session.user);
 });
 
 getNews();
